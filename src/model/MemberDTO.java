402f3a0001b9c68404bb54a4a4a92f94df14801f
kadun1 <kadunjj@gmail.com>
2020-11-26 18:58:44 +0900

    11/26수업커밋

--- src/model/MemberDTO.java
+++ src/model/MemberDTO.java
@@ -13,6 +13,22 @@ public class MemberDTO {
 	private String pass;
 	private String name;
 	private java.sql.Date regidate;
+	
+/*
+  	액션태그는 useBean을 통해 폼값을 한꺼번에 받기위해서는
+  	<form태그 하위 <input의 name속성과
+  	DTO객체의 멤버변수명이 반드시 일치해야 한다.
+ */	
+//	private int agt1;
+//	public int getAgt1() {
+//		return agt1;
+//	}
+//
+//	public void setAgt1(int agt1) {
+//		this.agt1 = agt1;
+//	}
+
+	
 	//기본생성자
 	public MemberDTO() {
 
@@ -50,6 +66,17 @@ public class MemberDTO {
 	}
 	public void setRegidate(java.sql.Date regidate) {
 		this.regidate = regidate;
+	}
+	
+	/*
+	 Object클래스에 정의된 메소드로 객체를 문자열형태로 변환해서
+	 반환해주는 역할을 한다. toString()을 오버라이딩하면 객체
+	 자체를 그대로 println() 하는것이 가능하다. 
+	 */
+	@Override
+	public String toString() {
+		return String.format("아이디:%s, 비밀번호:%s, 이름:%s",
+				id, pass, name);
 	}
 	
 }
